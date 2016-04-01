1e1c7c411ed52bfc28fee1830718f47753ac433f
Cubes School <school@cubes.rs>
2016-04-01 15:54:44 +0200

    LoginViewController almost completed.

--- ToDo/ToDo/LoginViewController.m
+++ ToDo/ToDo/LoginViewController.m
@@ -8,5 +8,51 @@
 
 #import "LoginViewController.h"
 
+@interface LoginViewController()
+@property (weak, nonatomic) IBOutlet UIImageView *usernameImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *passwordImageView;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@end
+
 @implementation LoginViewController
+
+#pragma mark - Private API
+
+- (void)configureTextField:(UITextField *)textField {
+    if (textField.placeholder.length > 0) {
+        
+//      UIColor *color = [UIColor colorWithRed:117.0/255.0
+//                                       green:113.0/255.0
+//                                        blue:111.0/255.0
+//                                       alpha:1.0];
+        
+        NSDictionary *attributes = @{
+                                     NSFontAttributeName: [UIFont fontWithName:@"AvenirNext-Regular" size:14.0],
+                                     NSForegroundColorAttributeName: [UIColor whiteColor]
+                                     };
+        textField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:textField.placeholder attributes:attributes];
+    }
+}
+
+#pragma mark - Actions
+
+- (IBAction)forgotPasswordButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)signInButtonTapped:(UIButton *)sender {
+}
+
+- (IBAction)signUpButtonTapped:(UIButton *)sender {
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self configureTextField:self.usernameTextField];
+    [self configureTextField:self.passwordTextField];
+}
+
 @end
